d909d5fb3e3d4bb46e9379c5a56dd4faa8e4c51f
Anselmo <memo_mister398@hotmail.com>
2021-11-24 11:41:35 -0300

    variables y constantes

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+// variables y constantes
+
+const nombre = 'Anselmo';
+const apellido = 'Cuevas';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+// var no se debe de usar...
+
+if ( true ) {
+    const nombre = 'Pepito'
+    let valorDado = 6;
+
+    console.log( nombre, valorDado );
+}
+
+console.log( valorDado );
