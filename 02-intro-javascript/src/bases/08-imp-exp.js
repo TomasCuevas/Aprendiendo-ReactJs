841d140c5916a926f280b3c87776992b4f36ec29
Anselmo <memo_mister398@hotmail.com>
2021-11-25 19:28:41 -0300

    promesas

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,15 +1,15 @@
-import heroes, { owners } from './data/heroes';
+import heroes, { owners } from '../data/heroes';
 
-console.log( owners );
+if ( owners ) // no sirve de nada
 
-const getHeroeById = ( id ) => {
+export const getHeroeById = ( id ) => {
     return heroes.find( ( heroe ) => heroe.id === id );
 }
 
-console.log( getHeroeById( 2 ) );
+// console.log( getHeroeById( 2 ) );
 
 const getHeroeByOwner = ( owner ) => {
     return heroes.filter( ( heroe ) => heroe.owner === owner );
 }
 
-console.log( getHeroeByOwner( 'Marvel' ) );
+// console.log( getHeroeByOwner( 'Marvel' ) );
