e7825c6d2294d1b8b397a4c6cbe92b8203b63b36
Anselmo <memo_mister398@hotmail.com>
2021-11-26 17:19:13 -0300

    ternario

---
+++ 02-intro-javascript/src/bases/12-ternario.js
@@ -0,0 +1,15 @@
+const activo = true;
+
+// const mensaje = '';
+
+// if ( activo ) {
+//     mensaje = 'Activo'
+// } else {
+//     mensaje = 'Inactivo'
+// }
+
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+
+const mensaje = activo && 'Activo';
+
+console.log( mensaje );
