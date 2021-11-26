7e1df137b722c0fbb5d54dd927fbfd3f51d22cd7
Anselmo <memo_mister398@hotmail.com>
2021-11-26 18:40:27 -0300

    hola mundo en reactjs 2.0

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const $divRoot = document.getElementById( 'root' );
+const saludo   = <h1> Hola Mundo </h1>;
+
+ReactDOM.render( saludo, $divRoot );
