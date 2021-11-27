fb8b9cd49a644efe2e2e1d0c935c9f0a43a62633
Anselmo <memo_mister398@hotmail.com>
2021-11-27 17:08:16 -0300

    retornar mas de un elemento en el componente - Fragment

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,8 +1,14 @@
 import React from 'react';
+// import { Fragment } from 'react';
 
 const PrimeraApp = () => {
 
-    return <h1>Hola Mundo</h1>
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primer componente</p>
+        </>
+    );
 }
 
 export default PrimeraApp
