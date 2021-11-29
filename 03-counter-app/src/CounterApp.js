859461bee6bc8b58abddba97432c6403a8762c6c
Anselmo <memo_mister398@hotmail.com>
2021-11-28 23:49:39 -0300

    evento click (eventos en general)

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ( { value } ) => {
 
+    // event handlers
+    const addValue = ( e ) => {
+        console.log( e );
+    }
+    
     return (
         <>
             <h1> CounterApp </h1>
             <h2> { value } </h2>
+            <button onClick={ addValue } > +1 </button>
         </>
     )
 }
