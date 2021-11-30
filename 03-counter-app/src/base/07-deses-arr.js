6065cce66df4158d49e4033e343657f3b8999128
Anselmo <memo_mister398@hotmail.com>
2021-11-30 10:19:49 -0300

    pruebas en el archivo 07-deses-arr.js

--- 03-counter-app/src/base/07-deses-arr.js
+++ 03-counter-app/src/base/07-deses-arr.js
@@ -1,14 +1,14 @@
 const personajes = [ 'Goku', 'Vegeta', 'Trunks' ];
 
 const [ , , p3 ] = personajes;
-console.log( p3 );
+// console.log( p3 );
 
-const retornaArreglo = () => {
+export const retornaArreglo = () => {
     return [ 'ABC', 123 ];
 }
 
 const [ letras, numeros ] = retornaArreglo();
-console.log( letras, numeros );
+// console.log( letras, numeros );
 
 const useState = ( valor ) => {
     return [ valor, () => { console.log( 'Hola Mundo' ) }]
@@ -16,5 +16,5 @@ const useState = ( valor ) => {
 
 const [ nombre, setNombre ] = useState( 'Goku' );
 
-console.log( nombre );
-setNombre();
+// console.log( nombre );
+// setNombre();
