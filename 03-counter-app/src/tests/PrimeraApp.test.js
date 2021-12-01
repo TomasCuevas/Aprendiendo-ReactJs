9b83c5793b50d597315b826695596d363de21cac
Anselmo <memo_mister398@hotmail.com>
2021-12-01 13:17:33 -0300

    revisar elementos dentro del componente

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -16,10 +16,27 @@ describe( 'Pruebas en <PrimeraApp />', () => {
     
     test('Debe de mostrar <PrimeraApp /> correctamente', () => {
         
-        const saludo = 'Hola, Juanito'
+        const saludo  = 'Hola, Juanito'
         const wrapper = shallow( <PrimeraApp saludo={ saludo } /> );
 
         expect( wrapper ).toMatchSnapshot(  );
     });
+    
+    test('Debe de mostrar el subitulo enviado por props', () => {
+        
+        const saludo    = 'Hola, Juanito';
+        const subtitulo = 'Es un subtitulo puesto por variable';
+
+        const wrapper   = shallow( 
+            <PrimeraApp 
+                saludo={ saludo }
+                subtitulo={ subtitulo }
+            />
+        );
+
+        const textoParrafo = wrapper.find( 'p' ).text();
+
+        expect( textoParrafo ).toBe( subtitulo );
+    })
     
 });
