ca51a2a670b490fee50771ad6aaeb64907f54645
Anselmo <memo_mister398@hotmail.com>
2021-12-01 13:05:01 -0300

    enzyme - testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+// import { render } from '@testing-library/react';
+
 import PrimeraApp from '../PrimeraApp';
 
 describe( 'Pruebas en <PrimeraApp />', () => {
 
-    test( 'Debe de mostrar el mensaje ( Hola, Juanito )', () => {
+    // test( 'Debe de mostrar el mensaje ( Hola, Juanito )', () => {
 
-        const saludo = 'Hola, Juanito';
+    //     const saludo = 'Hola, Juanito';
+    //     const { getByText } = render( <PrimeraApp saludo={ saludo } /> );
 
-        const { getByText } = render( <PrimeraApp saludo={ saludo } /> );
+    //     expect( getByText( saludo ) ).toBeInTheDocument(  )
+    // });
+    
+    test('Debe de mostrar <PrimeraApp /> correctamente', () => {
+        
+        const saludo = 'Hola, Juanito'
+        const wrapper = shallow( <PrimeraApp saludo={ saludo } /> );
 
-        expect( getByText( saludo ) ).toBeInTheDocument(  )
+        expect( wrapper ).toMatchSnapshot(  );
     });
     
 });
