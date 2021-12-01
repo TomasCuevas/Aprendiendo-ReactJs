569e6421f718742fbf3c8fd8982636100773d57c
Anselmo <memo_mister398@hotmail.com>
2021-12-01 16:51:55 -0300

    simular eventos - click

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -5,9 +5,14 @@ import CounterApp from '../CounterApp';
 
 describe( 'Pruebas en <CounterApp />', () => {
 
+    let wrapper = shallow( <CounterApp /> );
+
+    beforeEach( () => {
+
+        wrapper = shallow( <CounterApp /> );
+    })
+
     test( 'Debe mostrar CounterApp correctamente', () => {
-        
-        const wrapper = shallow( <CounterApp /> );
 
         expect( wrapper ).toMatchSnapshot();
     })
@@ -15,13 +20,34 @@ describe( 'Pruebas en <CounterApp />', () => {
     test( 'Debe mostrar el valor por defecto de 100', () => {
         
         const value = 100;
-
         const wrapper = shallow( <CounterApp value={ value } /> )
-
         const valueDefault = wrapper.find('h2').text();
 
         expect( valueDefault ).toBe( '100' );
     })
     
+    test( 'Debe de incrementar el valor con el boton +1', () => {
+
+        wrapper.find('button').at( 0 ).simulate( 'click' );
+        const counterText = wrapper.find( 'h2' ).text();
+
+        expect( counterText ).toBe( '1' );
+    });
+
+    test( 'Debe de disminuir el valor con el boton -1', () => {
+
+        wrapper.find('button').at( 2 ).simulate( 'click' );
+        const counterText = wrapper.find( 'h2' ).text();
+
+        expect( counterText ).toBe( '-1' );
+    });
+
+    test( 'Debe de resetear el valor con el boton reset', () => {
+
+        wrapper.find('button').at( 1 ).simulate( 'click' );
+        const counterText = wrapper.find( 'h2' ).text();
+
+        expect( counterText ).toBe( '0' );
+    });
     
 })
