b3f281682aa1b280c6b2fed5c8159db43e0aefbb
Anselmo Tomas Cuevas <memo_mister398@hotmail.com>
2021-12-04 23:27:01 -0300

    className - clases de css

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const GifGridItem = ({ id, title, url }) => {
-	
+
 	return (
-		<div>
+		<div className="card" >
 			<img src={ url } alt={ title } ></img>
 			<p>{ title }</p>
 		</div>
