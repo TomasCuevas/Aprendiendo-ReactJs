0333eef651a8c37a751652e2690571d7bad2bf9b
Anselmo Tomas Cuevas <memo_mister398@hotmail.com>
2021-12-04 22:56:08 -0300

    useEffect

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const GifGrid = ({ category }) => {
 	
+	const [count, setCount] = useState(0);
+	
+	useEffect( () => {
+		getGif();
+	}, []);
+
 	const getGif = async() => {
 
 		const url = 'http://api.giphy.com/v1/gifs/search?q=rick%20and%20morty&limit=10&api_key=qV5k5fNfElMQkPpD5DWCX7fVs5zPoHYe'
 
-		const res = await fetch( url )
+		const res = await fetch( url );
 		const { data } = await res.json();
 
 		const gifs = data.map( ( img ) => {
@@ -20,11 +26,11 @@ export const GifGrid = ({ category }) => {
 		console.log( gifs );
 	}
 
-	getGif();
-
 	return (
 		<div>
 			<h3>{ category }</h3>
+			<h3>{ count }</h3>
+			<button onClick={ () => setCount( count + 1 ) }>Boton</button>
 		</div>
 	)
 }
