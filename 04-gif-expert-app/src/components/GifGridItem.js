344da36493d3a6d82e9a2e21fb654539fcc4fdee
Anselmo Cuevas <memowin15@gmail.com>
2022-01-08 16:54:16 -0300

    implementando PropTypes y actualizar snapshots

--- 04-gif-expert-app/src/components/GifGridItem.js
+++ 04-gif-expert-app/src/components/GifGridItem.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+
 import 'animate.css';
 
-export const GifGridItem = ({ id, title, url }) => {
+export const GifGridItem = ({ title, url }) => {
 	
 	return (
 		<div className="card animate__animated animate__fadeInLeft" >
@@ -9,4 +12,9 @@ export const GifGridItem = ({ id, title, url }) => {
 			<p>{ title }</p>
 		</div>
 	)
+}
+
+GifGridItem.propTypes = {
+	title: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired
 }
--- 04-gif-expert-app/src/test/components/GifGridItem.test.js
+++ 04-gif-expert-app/src/test/components/GifGridItem.test.js
@@ -5,9 +5,12 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe( 'Prueba en <GridGifItem>', () => {
 
+    const title = 'Soy un titulo';
+    const url   = 'https://localhost/avatar.jpg';
+    
     test( 'Hacer snapshot de el componente', () => {
 
-        const wrapper = shallow( <GifGridItem /> )
+        const wrapper = shallow( <GifGridItem title={ title } url={ url } /> )
 
         expect( wrapper ).toMatchSnapshot();
     })
