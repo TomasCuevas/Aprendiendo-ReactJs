4819b710784530308b5a3b77d26a25010a084b55
TomasCuevas <memo_mister398@hotmail.com>
2022-01-10 05:18:42 -0300

    simular submit llamando la funcion y limpiando el inputValue

--- 04-gif-expert-app/src/test/components/AddCategory.test.js
+++ 04-gif-expert-app/src/test/components/AddCategory.test.js
@@ -26,9 +26,21 @@ describe( 'Prueba en <AddCategory />', () => {
     });
     
     test( 'No debe de postear la informacion con submit', () => {
-
         wrapper.find('form').simulate('submit', { preventDefault(){} })
 
         expect( setCategories ).not.toHaveBeenCalled();
+    });
+
+    test( 'Debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola Mundo';
+        
+        wrapper.find('input').simulate('change', { target: { value } });
+
+        wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+        expect( setCategories ).toHaveBeenCalledTimes(1);
+        expect( setCategories ).toHaveBeenCalled();
+        expect( setCategories ).toHaveBeenCalledWith( expect.any(Function) );
+        expect( wrapper.find('input').prop('value') ).toBe('');
     });
 })
