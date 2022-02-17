bbbe2a73cb35ceed325ef0152647296f23c1dc60
TomasCuevas <memo_mister398@hotmail.com>
2022-02-17 16:20:36 -0300

    memo - metodo de react

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -9,10 +9,11 @@ import ReactDOM from 'react-dom';
 // import { MultiplesCustomHooks } from './components/03-examples/MultiplesCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 import './index.css';
 
 ReactDOM.render(
-    <Layout />,
+    <Memorize />,
     document.getElementById('root')
 );
