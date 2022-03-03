0ba27dd5c604600004d8c065ee0a3edffc59a4d8
TomasCuevas <memo_mister398@hotmail.com>
2022-03-03 09:07:12 -0300

    creando el cascaron de la lista de ToDos

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -16,14 +16,43 @@ export const TodoApp = () => {
 
     return (
         <>
-            <h1>ToDo App</h1>
+            <h1>ToDo App ( {todos.length} ) </h1>
             <hr/>
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+            <div className='todo__container'>
+                <ul className='list__container'>
+                    {
+                        todos.map( ( todo, index ) => (
+                            <div
+                                className='list__item-container'
+                                key={todo.id}>
+                                <li
+                                    className='list__item'>
+                                    <p className='list__item-text'> {index + 1}. {todo.description} </p>
+                                    <button
+                                        className='list__delete-item'>
+                                        Borrar
+                                    </button>
+                                </li>
+                            </div>
+                        ))
+                    }
+                </ul>
+
+                <div className='add__todo-container'>
+                    <h4 className='add__todo-title'>Agregar ToDo</h4>
+                    <hr/>
+
+                    <form className='add__todo-form'>
+                        <input 
+                            placeholder='ToDo description'
+                            className='add__todo-input'
+                            type="text"
+                            name='description'/>
+                        <button className='add__todo-btn'>Agregar</button>
+                    </form>
+                </div>
+            </div>
         </>
     )
 }
