89702e7ebdff45e62d5709a86509e43c4bdfcd23
TomasCuevas <memo_mister398@hotmail.com>
2022-03-12 07:52:57 -0300

    createContext y useContext

--- 05-hook-app/src/components/09-useContext/MainApp.js
+++ 05-hook-app/src/components/09-useContext/MainApp.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
 
+    const user = {
+        id: 123,
+        name: 'Prueba1',
+        email: 'prueba1@test.com'
+    }
+    
     return (
-            <AppRouter />
+            <UserContext.Provider value={ user } >
+                <AppRouter />
+            </UserContext.Provider>
     )
 }
--- 05-hook-app/src/components/09-useContext/NavBar.js
+++ 05-hook-app/src/components/09-useContext/NavBar.js
@@ -14,13 +14,13 @@ export const NavBar = () => {
             
             <ul className='list__container'>
                 <li className='list__element'>
-                    <NavLink to="/" activeClassname='active' className='list__element-text'> Home </NavLink>
+                    <NavLink to="/" activeclassname='active' className='list__element-text'> Home </NavLink>
                 </li>
                 <li className='list__element'>
-                    <NavLink to="/about" activeClassname='active' className='list__element-text'> About </NavLink>
+                    <NavLink to="/about" activeclassname='active' className='list__element-text'> About </NavLink>
                 </li>
                 <li className='list__element'>
-                    <NavLink to="/login" activeClassname='active' className='list__element-text'> Login </NavLink>
+                    <NavLink to="/login" activeclassname='active' className='list__element-text'> Login </NavLink>
                 </li>
             </ul>
         </nav>
