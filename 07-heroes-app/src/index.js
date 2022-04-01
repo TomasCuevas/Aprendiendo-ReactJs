9b11aebce98fe43831f0a63864229923f31f3707
TomasCuevas <memo_mister398@hotmail.com>
2022-04-01 10:36:36 -0300

    inicio de proyecto - HeroesApp

---
+++ 07-heroes-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HeroesApp } from './HeroesApp';
+import './index.scss';
+
+ReactDOM.render(
+  <HeroesApp />,
+  document.getElementById('root')
+)
