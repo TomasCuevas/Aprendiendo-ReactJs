fe6754765d499fc537aa35a0804a96bcda065fc6
TomasCuevas <memo_mister398@hotmail.com>
2022-04-06 23:21:04 -0300

    aplicar queries y useSearchParams

--- 07-heroes-app/src/screen/SearchScreen/SearchScreen.jsx
+++ 07-heroes-app/src/screen/SearchScreen/SearchScreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { useForm } from '../../hooks/useForm';
 import { heroes } from '../../database/heroes';
@@ -7,16 +8,28 @@ import { HeroCard } from '../../components/HeroesList/components/HeroCard';
 import styles from './searchScreen.module.scss';
 
 export const SearchScreen = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const param = searchParams.get('hero') || '';
+
   const [inputValue, handleInputChange, reset  ] = useForm({
-    searchInput: ''
+    searchInput: param
   });
   const heroesFiltered = heroes;
+
+  
   
   const handleSearch = (e) => {
     e.preventDefault();
-    console.log(e.target.searchInput.value)
+
+    const hero = inputValue.searchInput.toLowerCase();
+    setSearchParams({ hero });
+
+    navigate(`/search?hero=${hero}`);
   }
+
   
+
   return (
     <>
       <h1>Search Screen</h1>
