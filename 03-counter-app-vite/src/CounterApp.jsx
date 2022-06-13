97c08fd85368c480c60d2a1f62ceb8a5772ae57c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-13 16:21:32 -0300

    handleSubtract y handleReset 18

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -4,19 +4,23 @@ import PropTypes from 'prop-types';
 export const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value);
 
-  const handleAdd = (e) => {
+  const handleAdd = () => {
     // setState(state + 1);
     setCounter((s) => s + 1);
   }
+
+  const handleSubstract = () => setCounter(counter - 1);
+
+  const handleReset = () => setCounter(value);
   
   return (
     <>
       <h1>Counter App</h1>
       <h2> { counter } </h2>
 
-      <button onClick={ handleAdd }>
-        +1
-      </button>
+      <button onClick={ handleAdd }> +1 </button>
+      <button onClick={ handleReset }> Reset </button>
+      <button onClick={ handleSubstract }> -1 </button>
     </>
   )
 }
