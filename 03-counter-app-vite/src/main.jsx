4e6f454bd7c7cdcfa2a2c5762e1b129c921cb169
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-13 14:47:52 -0300

    hola mundo en react 18

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,3 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const App = () => {
+
+  return <h1>Hola Mundo</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+);
