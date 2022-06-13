f94c4a9b999fa3a3a6bf5456f570aa93937890ee
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-13 15:24:11 -0300

    impresion de variables en el html 18

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,16 @@
+// const newMessage = {
+//   title: 'First Page',
+//   message: 'This is the first page of the app',
+// }
+
+// const saludo = 'Hola, que tal?'
+
 export const FirstApp = () => {
 
   return (
     <>
-      <h1>First App</h1>
+      <h1>Fist Page</h1>
+      {/* <code>{ JSON.stringify( newMessage ) }</code> */}
       <p>Soy un subtitulo</p>
     </>
   )
