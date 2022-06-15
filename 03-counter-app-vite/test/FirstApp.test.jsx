281df6c77c6e98b6103b77774e3a2fdb5a7dee8d
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-15 10:21:56 -0300

    probar <FirstApp /> 18

--- 03-counter-app-vite/test/FirstApp.test.jsx
+++ 03-counter-app-vite/test/FirstApp.test.jsx
@@ -5,7 +5,18 @@ describe('Pruebas en <FirstApp />', () => {
   
   test('debe de hacer match con el snapshot', () => {
     const title = 'Hola Mundo';
-    render(<FirstApp title={ title } />);
+    const { container } = render(<FirstApp title={ title } />);
+
+    expect( container ).toMatchSnapshot();
+  });
+
+  test('debe de mostrar el titulo en un h1', () => {
+    const title = 'Hola Mundo';
+    const { container, getByText } = render(<FirstApp title={ title } />);
+    const h1 = container.querySelector('h1');
+
+    expect( getByText(title) ).toBeTruthy();
+    expect( h1.innerHTML ).toContain(title);
   });
 
 });
