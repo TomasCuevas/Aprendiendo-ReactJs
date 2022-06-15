d965c2113d575a01359522d38a68abb444cbf33c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-15 09:19:33 -0300

    pruebas con async-await & configuraciones de jest 18

--- 03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -1,32 +1,17 @@
-
-
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+        const apiKey = 'ENn8GXJ9NZwet2ZprGgi4c67WUJwK6MJ';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        const { url } = await data.images.original;
+        
+        return url;
 
     } catch (error) {
         // manejo del error
-        console.error(error)
-    }
-    
-    
-    
+        return 'No se encontro la imagen.';
+    }   
 }
-
- getImagen();
-
-
-
