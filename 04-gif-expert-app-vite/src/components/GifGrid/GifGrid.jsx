185d97bb6917d87fcc14d0c91dedd3ed87dd6400
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-16 12:21:58 -0300

    useEffect 18

--- 04-gif-expert-app-vite/src/components/GifGrid/GifGrid.jsx
+++ 04-gif-expert-app-vite/src/components/GifGrid/GifGrid.jsx
@@ -1,12 +1,20 @@
+import { useState, useEffect } from 'react';
 import { getGifs } from '../../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
+  const [ count, setCount ] = useState(0);
 
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, [])
   
   return (
     <>
       <h3>{ category }</h3>
+
+      <h4>{ count }</h4>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+
     </>
   )
 }
