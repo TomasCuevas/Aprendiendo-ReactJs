602660605fc16aaedbf46a5ce440e57c14e6a537
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-16 12:39:47 -0300

    mostrar los titulos de las imagenes 18

--- 04-gif-expert-app-vite/src/components/GifGrid/GifGrid.jsx
+++ 04-gif-expert-app-vite/src/components/GifGrid/GifGrid.jsx
@@ -2,19 +2,29 @@ import { useState, useEffect } from 'react';
 import { getGifs } from '../../helpers/getGifs';
 
 export const GifGrid = ({ category }) => {
-  const [ count, setCount ] = useState(0);
+  const [ gifs, setGifs ] = useState([]);
 
+  const getCurrentGifs = async() => {
+    const gifs = await getGifs(category);
+    setGifs(gifs);
+  }
+  
   useEffect(() => {
-    getGifs(category);
+    getCurrentGifs();
   }, [])
   
   return (
-    <>
+    <section>
       <h3>{ category }</h3>
 
-      <h4>{ count }</h4>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <ol>
+        {
+          gifs.map( ({ id, title, url }) => (
+            <li key={id}>{ title }</li>
+          ))
+        }
+      </ol>
 
-    </>
+    </section>
   )
 }
