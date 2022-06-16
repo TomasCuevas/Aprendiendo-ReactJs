f903685ef511a61613c1f0fdb57d6fb8c86686dd
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-16 10:02:08 -0300

    inicio de proyecto - GifExpertApp 18

---
+++ 04-gif-expert-app-vite/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifExpertApp } from './GifExpertApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GifExpertApp />
+)
