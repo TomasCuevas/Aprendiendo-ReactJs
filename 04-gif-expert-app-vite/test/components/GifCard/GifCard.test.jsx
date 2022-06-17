c5f0a233fab5e89845c7da58873caaff1ddb1ace
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-17 13:45:37 -0300

    pruebas del componente - GifCard 18

--- 04-gif-expert-app-vite/test/components/GifCard/GifCard.test.jsx
+++ 04-gif-expert-app-vite/test/components/GifCard/GifCard.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifCard } from "../../../src/components/GifCard/GifCard";
 
 describe('Pruebas en <GifCard />', () => {
@@ -6,9 +6,26 @@ describe('Pruebas en <GifCard />', () => {
   const title = 'Titulo del GIF';
   const url = 'https://urldelgif.com';
   
-  test('hacer snapshot del componente', () => {
+  test('debe de hacer match con el snapshot', () => {
     const { container } = render(<GifCard title={title} url={url} />);
-    expect(container).toMatchSnapshot();
+    expect( container ).toMatchSnapshot();
   });
-  
+
+  test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GifCard title={title} url={url} />);
+    
+    // expect( screen.getByRole('img').src ).toBe( `${url}/` );
+    // expect( screen.getByRole('img').alt ).toBe( title );
+
+    const { src, alt } = screen.getByRole('img');
+
+    expect( src ).toBe( `${url}/` );
+    expect( alt ).toBe( title );
+  });
+
+  test('debe de mostrar el titulo con el texto indicado', () => {
+    render(<GifCard title={title} url={url} />);
+    expect( screen.getByText(title) ).toBeTruthy();
+  })
+
 })
