1176b2032cd7c4ac6d70cbb34150e775c8244575
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-18 10:14:41 -0300

    jest functions 18

--- 04-gif-expert-app-vite/test/components/AddCategory/AddCategory.test.jsx
+++ 04-gif-expert-app-vite/test/components/AddCategory/AddCategory.test.jsx
@@ -15,7 +15,9 @@ describe('Pruebas en el componente <AddCategory />', () => {
   });
 
   test('debe de llamar onNewCategory si el input tiene un valor', () => {
-    render( <AddCategory onNewCategory={() => {}} /> );
+    const onNewCategory = jest.fn();
+    
+    render( <AddCategory onNewCategory={onNewCategory} /> );
     const input = screen.getByRole('textbox');
     const form = screen.getByRole('form');
 
@@ -23,6 +25,22 @@ describe('Pruebas en el componente <AddCategory />', () => {
     fireEvent.submit( form );
 
     expect( input.value ).toBe( '' );
+
+    expect( onNewCategory ).toHaveBeenCalled();
+    expect( onNewCategory ).toHaveBeenCalledTimes( 1 );
+    expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+  });
+
+  test('no debe de llamar onNewCategory si el input esta vacio', () => {
+    const onNewCategory = jest.fn();
+    
+    render( <AddCategory onNewCategory={onNewCategory} /> );
+    const form = screen.getByRole('form');
+
+    fireEvent.submit( form );
+
+    expect( onNewCategory ).toHaveBeenCalledTimes( 0 );
+    expect( onNewCategory ).not.toHaveBeenCalled();
   });
 
 })
