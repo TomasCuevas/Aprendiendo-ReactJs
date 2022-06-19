62a3d68c1de13e95d5439fe7ca826fef350bfe80
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-19 13:16:08 -0300

    exponer metodos del Hook 18

--- 05-hook-app-vite/src/hooks/useCounter.js
+++ 05-hook-app-vite/src/hooks/useCounter.js
@@ -1,9 +1,25 @@
 import { useState } from 'react';
 
-export const useCounter = ( initialState = 0 ) => {
-  const [ counter, setCounter ] = useState(initialState);
+export const useCounter = ( initialValue = 0 ) => {
+  const [ counter, setCounter ] = useState(initialValue);
 
+  const increment = ( value = 1 ) => setCounter( counter + value );
+
+  const decrement = ( value = 1 ) => {
+    if (counter - value <= 0) {
+      setCounter(0);
+      return;
+    }
+
+    setCounter( counter - value )
+  };
+
+  const reset = () => setCounter( initialValue );
+  
   return {
-    counter
+    counter,
+    increment,
+    decrement,
+    reset
   }
 }
