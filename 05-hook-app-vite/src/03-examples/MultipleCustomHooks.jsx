a379a73340d0f995d67db5608b007edca5679662
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-20 10:03:39 -0300

    useFetch + useCounter 18

--- 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,31 @@
-import { useFetch } from "../hooks/useFetch"
+import { useFetch } from '../hooks/useFetch';
 
 export const MultipleCustomHooks = () => {
   const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
 
-  console.log({ data, isLoading, hasError });
+  const { quote, author } = !!data && data[0];
 
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
 
+      { (isLoading) && (<div className="alert alert-info text-center">Loading...</div>)}
+
+      { (!isLoading) && (
+        <blockquote className="blockquote text-end">
+          <p className="mb-10">
+            { quote }
+          </p>
+          <footer className="blockquote-footer">
+            { author }
+          </footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary">
+        Next Quote
+      </button>
     </>
   )
 }
