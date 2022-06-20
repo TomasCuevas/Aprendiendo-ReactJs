0f97a5db1a5c91779eedae735dd469aa599d385e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-20 11:13:27 -0300

    useLayoutEffect 18

---
+++ 05-hook-app-vite/src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,32 @@
+import { useCounter, useFetch } from '../hooks';
+import { LoadingQuote, Quote, QuoteButtoms } from '../03-examples';
+
+export const Layout = () => {
+  const { counter, increment, decrement } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+  const { quote, author } = !!data && data[0];
+
+  return (
+    <>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+
+      { (isLoading) && <LoadingQuote />}
+
+      { (!isLoading) && (
+        <>
+          <Quote 
+            quote={quote} 
+            author={author} 
+          />
+          <QuoteButtoms 
+            counter={counter} 
+            increment={increment} 
+            decrement={decrement} 
+          />
+        </>
+      )}
+    </>
+  )
+}
