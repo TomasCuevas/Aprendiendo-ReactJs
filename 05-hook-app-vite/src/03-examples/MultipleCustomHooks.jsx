a4cfd32112da1759851e913929db36becf141c48
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-20 10:16:47 -0300

    conectando ambos Hooks 18

--- 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app-vite/src/03-examples/MultipleCustomHooks.jsx
@@ -1,7 +1,9 @@
+import { useCounter } from '../hooks/useCounter';
 import { useFetch } from '../hooks/useFetch';
 
 export const MultipleCustomHooks = () => {
-  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+  const { counter, increment, decrement } = useCounter(1);
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
 
   const { quote, author } = !!data && data[0];
 
@@ -13,19 +15,31 @@ export const MultipleCustomHooks = () => {
       { (isLoading) && (<div className="alert alert-info text-center">Loading...</div>)}
 
       { (!isLoading) && (
-        <blockquote className="blockquote text-end">
-          <p className="mb-10">
-            { quote }
-          </p>
-          <footer className="blockquote-footer">
-            { author }
-          </footer>
-        </blockquote>
+        <>
+          <blockquote className="blockquote text-end">
+            <p className="mb-10">
+              { quote }
+            </p>
+            <footer className="blockquote-footer">
+              { author }
+            </footer>
+          </blockquote>
+          
+          <button 
+            className="btn btn-primary"
+            onClick={() => decrement()}
+            disabled={counter === 1 && true}
+          >
+            Previous Quote
+          </button>
+          <button 
+            className="btn btn-primary"
+            onClick={() => increment()}
+          >
+            Next Quote
+          </button>
+        </>
       )}
-
-      <button className="btn btn-primary">
-        Next Quote
-      </button>
     </>
   )
 }
