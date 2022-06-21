31d8195afa793ce211ef687df799826cfa3a6bca
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 12:17:32 -0300

    NavLink 18

--- 05-hook-app-vite/src/09-useContext/Navbar.jsx
+++ 05-hook-app-vite/src/09-useContext/Navbar.jsx
@@ -1,12 +1,25 @@
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export const Navbar = () => {
 
   return (
-    <>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-    </>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">useContext</Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">Login</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
