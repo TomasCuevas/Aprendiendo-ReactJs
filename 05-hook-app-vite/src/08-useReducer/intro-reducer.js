f606ecb7a9a652e24c32cceebc3754d05e49bb14
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 08:56:05 -0300

    idea general de un reducer 18

---
+++ 05-hook-app-vite/src/08-useReducer/intro-reducer.js
@@ -0,0 +1,28 @@
+const initialState = [{
+  id: 1,
+  todo: 'Recolectar la game del alma',
+  done: false
+}];
+
+const todoReducer = ( state = initialState, action = {} ) => {
+
+  if ( action.type === 'ADD_TODO' ) return [ ...state, action.payload ];
+  
+  return state;
+}
+
+let todos = todoReducer();
+const newTodo = {
+  id: 2,
+  todo: 'Recolectar la gema del infinito',
+  done: false
+}
+
+const addTodoAction = {
+  type: 'ADD_TODO',
+  payload: newTodo
+}
+
+todos = todoReducer( todos, addTodoAction );
+
+console.log({ state: todos });
