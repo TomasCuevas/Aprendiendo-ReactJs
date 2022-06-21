a468188f770fc236e5a8fe0e7a890f601692c81e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 09:21:18 -0300

    creando el cascaron de la lista de ToDos 18

--- 05-hook-app-vite/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app-vite/src/08-useReducer/TodoApp.jsx
@@ -23,14 +23,40 @@ export const TodoApp = () => {
 
   return (
     <>
-      <h1>ToDo App</h1>
+      <h1>ToDo App: 10 <small>pendientes: 2</small></h1>
       <hr />
 
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+      <div className="row">
+
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger">Borrar</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar ToDo</h4>
+          <hr />
+          <form>
+            <input 
+              type="text" 
+              placeholder="¿Qué hay que hacer?"
+              className="form-control"
+            />
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+
+      </div>
+
     </>
   )
 }
