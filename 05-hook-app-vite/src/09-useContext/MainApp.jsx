0502ef35685f457d4a66dbf863cfaeb94d415dc3
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-21 11:58:29 -0300

    configurar Router en React 18

--- 05-hook-app-vite/src/09-useContext/MainApp.jsx
+++ 05-hook-app-vite/src/09-useContext/MainApp.jsx
@@ -1,9 +1,27 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+/**
+ * @pages
+ */
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { AboutPage } from './AboutPage';
+
 export const MainApp = () => {
 
   return (
     <>
       <h1>Main App</h1>
       <hr />
+
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/login' element={ <LoginPage /> } />
+        <Route path='/about' element={ <AboutPage /> } />
+
+        
+        <Route path='/*' element={ <Navigate to='/' /> } />
+      </Routes>
     </>
   )
 }
