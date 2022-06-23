96c2e460cf06e5a805cc55485815a0e4791dcec1
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 08:44:52 -0300

    creando un primer Router 18

--- 07-heroes-app-vite/src/main.jsx
+++ 07-heroes-app-vite/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 
 import { HeroesApp } from './HeroesApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HeroesApp />
+  <BrowserRouter>
+    <HeroesApp />
+  </BrowserRouter>
 )
