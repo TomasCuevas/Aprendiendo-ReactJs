946579db4836d4ed6e3db0ffc352855e950fa739
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 11:52:42 -0300

    diseño de la pantalla de busqueda 18

--- 07-heroes-app-vite/src/heroes/pages/SearchPage.jsx
+++ 07-heroes-app-vite/src/heroes/pages/SearchPage.jsx
@@ -1,9 +1,49 @@
+import { HeroCard } from '../components';
+
 export const SearchPage = () => {
 
   return (
     <>
-      <h1>SearchPage</h1>
+      <h1>Search</h1>
       <hr />
+
+      <div className="row">
+        <div className="col-5 mt-5">
+          <h4>Searching</h4>
+          <hr />
+
+          <form>
+            <input 
+              type="text" 
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+            <button className="btn btn-outline-primary">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7 mt-5">
+          <h4>Result</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            Not hero with <b>ABC</b>
+          </div>
+
+          {/* <HeroCard /> */}
+
+        </div>
+      </div>
+
     </>
   )
 }
