35c2676d8b41f6eb3c370abdf6b8d6379169b1cc
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 09:01:37 -0300

    colocar clase de la ruta activa 18

--- 07-heroes-app-vite/src/router/AppRouter.jsx
+++ 07-heroes-app-vite/src/router/AppRouter.jsx
@@ -1,24 +1,30 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 /**
+ * @components
+ */
+import { Navbar } from '../ui';
+
+/**
  * @pages
  */
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
-import { LoginPage } from '../auth/pages/LoginPage';
+import { MarvelPage, DcPage } from '../heroes';
+import { LoginPage } from '../auth';
 
 export const AppRouter = () => {
 
   return (
     <>
+      <Navbar />
+    
       <Routes>
         
-      <Route path='/marvel' element={ <MarvelPage /> } />
-      <Route path='/dc' element={ <DcPage /> } />
+        <Route path='/marvel' element={ <MarvelPage /> } />
+        <Route path='/dc' element={ <DcPage /> } />
 
-      <Route path='/login' element={ <LoginPage /> } />
+        <Route path='/login' element={ <LoginPage /> } />
 
-      <Route path='/*' element={ <Navigate to='/marvel' /> } />
+        <Route path='/*' element={ <Navigate to='/marvel' /> } />
 
       </Routes>
     </>
