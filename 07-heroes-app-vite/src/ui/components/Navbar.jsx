b0a5874fbfb381288f4b0046ae30dfe88c2d046f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 09:45:21 -0300

    navigate push / replace - useNavigate 18

--- 07-heroes-app-vite/src/ui/components/Navbar.jsx
+++ 07-heroes-app-vite/src/ui/components/Navbar.jsx
@@ -1,6 +1,13 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true
+    })
+  };
   
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -46,6 +53,7 @@ export const Navbar = () => {
 
           <button
             className="nav-item nav-link btn"
+            onClick={ handleLogout }
           >
             Logout
           </button>
