69c636b8b251d407bc538ab8c43ec2b803311284
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-23 09:33:38 -0300

    creando un segundo Router 18

---
+++ 07-heroes-app-vite/src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+/**
+ * @components
+ */
+import { Navbar } from '../../ui';
+
+/**
+ * @pages
+ */
+import { DcPage, MarvelPage, SearchPage, HeroPage } from '../';
+
+export const HeroesRoutes = () => {
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <Routes>
+          
+          <Route path='/marvel' element={ <MarvelPage /> } />
+          <Route path='/dc' element={ <DcPage /> } />
+          <Route path='/search' element={ <SearchPage /> } />
+          <Route path='/hero' element={ <HeroPage /> } />
+          <Route path='/*' element={ <Navigate to='/marvel' /> } />
+
+        </Routes>
+      </div>
+    </>
+  )
+}
--- 07-heroes-app-vite/src/router/AppRouter.jsx
+++ 07-heroes-app-vite/src/router/AppRouter.jsx
@@ -1,30 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-
-/**
- * @components
- */
-import { Navbar } from '../ui';
+import { Routes, Route } from 'react-router-dom';
 
 /**
  * @pages
  */
-import { MarvelPage, DcPage } from '../heroes';
+import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 
 export const AppRouter = () => {
 
   return (
     <>
-      <Navbar />
-    
       <Routes>
         
-        <Route path='/marvel' element={ <MarvelPage /> } />
-        <Route path='/dc' element={ <DcPage /> } />
-
         <Route path='/login' element={ <LoginPage /> } />
-
-        <Route path='/*' element={ <Navigate to='/marvel' /> } />
+        <Route path='/*' element={ <HeroesRoutes /> } />
 
       </Routes>
     </>
