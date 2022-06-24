8dad1b65ca466eea1a3d953ba3316138397aad78
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-24 11:22:32 -0300

    pruebas en el PublicRoute - parte2 18

--- 07-heroes-app-vite/test/router/PublicRouter.test.jsx
+++ 07-heroes-app-vite/test/router/PublicRouter.test.jsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { AuthContext } from "../../src/auth";
+import { MarvelPage } from "../../src/heroes/pages/MarvelPage";
 import { PublicRouter } from "../../src/router/PublicRouter";
 
 describe('Pruebas en el <PublicRouter />', () => {
 
-  const contextValues = {
-    logged: false
-  }
+  const user = { id: '123', name: 'Tomas Cuevas' };
   
   test('debe de mostrar el children si no esta autenticado', () => {
+    const contextValues = { logged: false };
+
     render(
       <AuthContext.Provider value={{ contextValues }}>
         <PublicRouter>
@@ -18,6 +20,29 @@ describe('Pruebas en el <PublicRouter />', () => {
     );
 
     expect( screen.getByText('Ruta Publica') ).toBeTruthy();
+  });
+
+  test('debe de navegar si esta autenticado', () => {
+    const contextValues = { logged: true, user };
+    
+    render(
+      <AuthContext.Provider value={ contextValues }>
+        <MemoryRouter initialEntries={['/login']}>
+
+          <Routes>
+            <Route path='/login' element={
+              <PublicRouter>
+                <h1>Ruta Publica</h1>
+              </PublicRouter>
+            } />
+            <Route path='/marvel' element={ <h1>Pagina de Marvel</h1> } />
+          </Routes>
+          
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+    
+    expect( screen.getByText('Pagina de Marvel') ).toBeTruthy();
   });
 
 })
