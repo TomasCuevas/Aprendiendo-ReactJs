a222c894377e366b57d931d2c072f36e6375e13e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-24 09:03:06 -0300

    mantener el usuario activo 18

--- 07-heroes-app-vite/src/auth/context/AuthProvider.jsx
+++ 07-heroes-app-vite/src/auth/context/AuthProvider.jsx
@@ -3,23 +3,28 @@ import { types } from '../types/types';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 
-const initialState = {
-  logged: false
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user,
+    user
+  } 
 }
 
 export const AuthProvider = ({ children }) => {
-  const [ authState, dispatch ] = useReducer( authReducer, initialState )
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init )
 
   const login = ( name ) => {
+    const user = { id: 123, name };
+    
     const action = {
       type: types.login,
-      payload: {
-        id: 123,
-        name
-      }
+      payload: user
     }
 
     dispatch( action );
+    localStorage.setItem( 'user', JSON.stringify(user) );
   }
 
   return (
