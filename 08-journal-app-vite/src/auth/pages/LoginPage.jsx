53dfc9e8529b660bca0ea22d0a58914933dc39f6
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-25 09:51:32 -0300

    LoginPage - diseño sin layout parte2 18

--- 08-journal-app-vite/src/auth/pages/LoginPage.jsx
+++ 08-journal-app-vite/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, Typography, TextField } from "@mui/material"
+import { Grid, Typography, TextField, Button, Link } from "@mui/material"
+import { Google } from "@mui/icons-material"
+import { Link as Routerlink } from "react-router-dom"
 
 export const LoginPage = () => {
 
@@ -18,6 +20,7 @@ export const LoginPage = () => {
       >
         <Typography variant='5' sx={{ mb: 1 }}>Login</Typography>
         <form>
+
           <Grid container>
             <Grid item xs={ 12 } sx={{ mt: 2 }}>
               <TextField label='Correo' type='email' placeholder='correo@google.com' fullWidth />
@@ -26,6 +29,27 @@ export const LoginPage = () => {
               <TextField label='Contraseña' type='password' placeholder='contraseña' fullWidth />
             </Grid>
           </Grid>
+          
+          <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={ 12 } sm={ 6 }>
+              <Button variant='contained' fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={ 12 } sm={ 6 }>
+              <Button variant='contained' fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Link component={ Routerlink } to='/auth/register'>
+              Crear una cuenta
+            </Link>
+          </Grid>
+
         </form>
       </Grid>
     </Grid>
