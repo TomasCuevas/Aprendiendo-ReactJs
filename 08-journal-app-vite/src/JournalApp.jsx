49c6ed705fd2d19be0e139a2077ef04ee8b08c82
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-25 09:05:12 -0300

    configuracion de MUI con Vite 18

--- 08-journal-app-vite/src/JournalApp.jsx
+++ 08-journal-app-vite/src/JournalApp.jsx
@@ -1,10 +1,11 @@
 import { AppRouter } from "./router/AppRoutes"
+import { AppTheme } from "./themes/AppTheme"
 
 export const JournalApp = () => {
 
   return (
-    <>
+    <AppTheme>
       <AppRouter />
-    </>
+    </AppTheme>
   )
 }
