d742cfa44bf95bdeed9589b504555bef67904349
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-27 12:54:45 -0300

    pokemonSlice 18

--- 09-redux-toolkit/src/main.jsx
+++ 09-redux-toolkit/src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { App } from "./App";
+import { PokemonApp } from "./PokemonApp";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PokemonApp />
   </Provider>
 );
