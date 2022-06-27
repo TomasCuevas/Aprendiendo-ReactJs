cf4f26a194e9b6c5c74357e05949d84128e957a9
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-27 11:19:12 -0300

    configureStore y slices 18

---
+++ 09-redux-toolkit/src/store/slices/counter/counterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    counter: 10,
+  },
+  reducers: {
+    increment: (state) => {
+      state.counter += 1;
+    },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
