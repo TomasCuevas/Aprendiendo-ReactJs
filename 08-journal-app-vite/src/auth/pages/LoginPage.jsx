4e76c8291ee208c32203dce79b64f67d16d4ba38
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-28 10:12:59 -0300

    disparar accion de autenticacion 18

--- 08-journal-app-vite/src/auth/pages/LoginPage.jsx
+++ 08-journal-app-vite/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from "react-redux";
+import { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link as Routerlink } from "react-router-dom";
 import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 import { Google } from "@mui/icons-material";
@@ -13,10 +14,13 @@ import {
 
 export const LoginPage = () => {
   const dispatch = useDispatch();
+  const { status } = useSelector((state) => state.auth);
   const { formState, email, password, onInputChange } = useForm({
     email: "memowin15@gmail.com",
     password: "12345678",
   });
+
+  const isAuthenticating = useMemo(() => status === "checking", [status]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -57,12 +61,22 @@ export const LoginPage = () => {
 
         <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
           <Grid item xs={12} sm={6}>
-            <Button type="submit" variant="contained" fullWidth>
+            <Button
+              disabled={isAuthenticating}
+              type="submit"
+              variant="contained"
+              fullWidth
+            >
               Login
             </Button>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <Button onClick={onGoogleSignin} variant="contained" fullWidth>
+            <Button
+              disabled={isAuthenticating}
+              onClick={onGoogleSignin}
+              variant="contained"
+              fullWidth
+            >
               <Google />
               <Typography sx={{ ml: 1 }}>Google</Typography>
             </Button>
