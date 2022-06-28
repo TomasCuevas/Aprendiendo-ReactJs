baa5096b25453266bdd19652acaf39593ef45345
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-28 08:33:58 -0300

    configurar el AuthSlice 18

--- 08-journal-app-vite/src/store/store.js
+++ 08-journal-app-vite/src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { authSlice } from "./auth/authSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice.reducer,
+  },
 });
