644ceafa65b49db97843007c4a5d902d9949f572
Tomas Cuevas <memo_mister398@hotmail.com>
2022-06-29 10:35:14 -0300

    crear una nueva nota 18

--- 08-journal-app-vite/src/journal/pages/JournalPage.jsx
+++ 08-journal-app-vite/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,16 @@
-import { IconButton } from '@mui/material'
-import { AddOutlined } from '@mui/icons-material'
-import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView, NothingSelectedView } from '../views'
+import { useDispatch } from 'react-redux';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
+import { startNewNote } from '../../store/journal/thunks';
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
 
   return (
     <JournalLayout>
@@ -11,18 +18,19 @@ export const JournalPage = () => {
       {/* <NoteView /> */}
 
       <IconButton
+        onClick={onClickNewNote}
         size='large'
-        sx={{ 
-          color: 'primary.main', 
-          backgroundColor: 'error.main', 
-          ':hover': { backgroundColor: 'error.main', opacity: 0.9 } ,
+        sx={{
+          color: 'primary.main',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
           position: 'fixed',
           right: 50,
-          bottom: 50
+          bottom: 50,
         }}
       >
         <AddOutlined sx={{ fontSize: 50, color: 'white' }} />
       </IconButton>
     </JournalLayout>
-  )
-}
+  );
+};
