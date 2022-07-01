0c3fea35a5f61588601212b2202643f750c13d48
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-01 12:09:15 -0300

    Cloudinary SDK - delete image 18

--- 08-journal-app-vite/tests/journal/helpers/fileUpload.test.js
+++ 08-journal-app-vite/tests/journal/helpers/fileUpload.test.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-undef */
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from '../../../src/journal/helpers/fileUpload';
+
+cloudinary.config({
+  cloud_name: 'dn3kl3egc',
+  api_key: '831726713172783',
+  api_secret: 'sCIrymZykTJmhOsmjbwp5Y4e2y4',
+  secure: true,
+});
 
 describe('Pruebas en fileUpload', () => {
   jest.setTimeout(15000);
@@ -13,5 +21,17 @@ describe('Pruebas en fileUpload', () => {
 
     const url = await fileUpload(file);
     expect(typeof url).toBe('string');
+
+    const segments = url.split('/');
+    const imageId = segments[segments.length - 1].replace('.png', '');
+
+    await cloudinary.api.delete_resources([`journal-app/${imageId}`]);
+  });
+
+  test('debe de retornar null', async () => {
+    const file = new File([], 'image.jpg');
+    const url = await fileUpload(file);
+
+    expect(url).toBe(null);
   });
 });
