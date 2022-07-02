758d2a7e2b4f42ab82cc426eb12f355718e6d448
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-02 12:44:03 -0300

    thunks - checkingCredentials correcto e incorrecto 18

--- 08-journal-app-vite/tests/store/auth/thunks.test.js
+++ 08-journal-app-vite/tests/store/auth/thunks.test.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
-import { checkingCredentials } from '../../../src/store/auth/authSlice';
-import { startCheckingAuthentication } from '../../../src/store/auth/thunks';
+import { signInWithGoogle } from '../../../src/firebase/providers';
+import { checkingCredentials, login, logout } from '../../../src/store/auth/authSlice';
+import { startCheckingAuthentication, startGoogleSignin } from '../../../src/store/auth/thunks';
+import { demoUser } from '../../fixtures/authFixtures';
 
 jest.mock('../../../src/firebase/providers');
 
@@ -14,5 +16,24 @@ describe('Pruebas en authThunks', () => {
     await startCheckingAuthentication()(dispatch);
 
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+
+  test('startGoogleSignin debe de llamar checkingCredentials y login - exito', async () => {
+    const loginData = { ok: true, ...demoUser };
+    await signInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleSignin()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test('startGoogleSignin debe de llamar checkingCredentials y logout - error', async () => {
+    const errorMessage = 'Error al realizar el login.';
+    const loginData = { ok: false, errorMessage };
+    await signInWithGoogle.mockResolvedValue(loginData);
+    await startGoogleSignin()(dispatch);
+
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(logout(errorMessage));
   });
 });
