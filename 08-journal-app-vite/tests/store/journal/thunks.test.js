9247581df451ef2edfd265453ccc2f09d0426840
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-02 14:49:50 -0300

    prueba completa sobre insercion (not found) 18

--- 08-journal-app-vite/tests/store/journal/thunks.test.js
+++ 08-journal-app-vite/tests/store/journal/thunks.test.js
@@ -1,4 +1,11 @@
 /* eslint-disable no-undef */
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../../src/firebase/config';
+import {
+  addNewEmptyNote,
+  savingNewNote,
+  setActiveNote,
+} from '../../../src/store/journal/journalSlice';
 import { startNewNote } from '../../../src/store/journal/thunks';
 
 describe('Pruebas en journal thunks', () => {
@@ -14,5 +21,30 @@ describe('Pruebas en journal thunks', () => {
     getState.mockReturnValue({ auth: { uid } });
 
     await startNewNote()(dispatch, getState);
+    expect(dispatch).toHaveBeenCalledWith(savingNewNote());
+    expect(dispatch).toHaveBeenCalledWith(
+      addNewEmptyNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+      }),
+    );
+    expect(dispatch).toHaveBeenCalledWith(
+      setActiveNote({
+        body: '',
+        title: '',
+        id: expect.any(String),
+        date: expect.any(Number),
+      }),
+    );
+
+    // Borrar de firebase
+    const collectionRef = collection(FirebaseDB, `${uid}/journal/notes`);
+    const docs = await getDocs(collectionRef);
+
+    const deletePromises = [];
+    docs.forEach((doc) => deletePromises.push(deleteDoc(doc.ref)));
+    await Promise.all(deletePromises);
   });
 });
