ed4fc2a8d17410b85cc0d45ee11e50872177a557
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-04 09:48:08 -0300

    escuchar eventos del calendario 18

--- 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
@@ -5,6 +5,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Navbar, CalendarEventBox } from '../';
 
 import { localizer, getMessagesES } from '../../helpers';
+import { useState } from 'react';
 
 const events = [
   {
@@ -20,6 +21,8 @@ const events = [
 ];
 
 export const CalendarPage = () => {
+  const [lastView, setLastView] = useState(localStorage.getItem('lastview') || 'week');
+
   const eventStylesGetter = (event, start, end, isSelected) => {
     const style = {
       backgroundColor: '#347CF7',
@@ -33,6 +36,19 @@ export const CalendarPage = () => {
     };
   };
 
+  const onDoubleClick = (event) => {
+    console.log({ doubleClick: event });
+  };
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+  };
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastview', event);
+    console.log({ viewChange: event });
+  };
+
   return (
     <>
       <Navbar />
@@ -42,12 +58,16 @@ export const CalendarPage = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        defaultView={lastView}
         style={{ height: 'calc(100vh - 80px)' }}
         messages={getMessagesES()}
         eventPropGetter={eventStylesGetter}
         components={{
           event: CalendarEventBox,
         }}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   );
