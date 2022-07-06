c186572a8bd03277fca50401c1f79cf413ff9bd3
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-06 17:03:50 -0300

    configurar CORS 18

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const { dbConnection } = require("./database/config");
@@ -13,6 +14,7 @@ dbConnection();
 /**
  * @middlewares
  */
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 
