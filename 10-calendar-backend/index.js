59fbb4c75b4caf9945f322862eff4bf77deeb999
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-06 08:34:17 -0300

    variables de entorno y carpeta publica 18

--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -1,13 +1,10 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+app.use(express.static("public"));
 
-app.listen(4000, () => {
-  console.log("Servidor corriendo en el puerto 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
 });
