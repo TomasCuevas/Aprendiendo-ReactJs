d02c2f789a2ea29a8c824c950081e468e166b4e2
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-08 16:34:00 -0300

    despachar acciones respectivas 18

--- 10-calendar-app-vite/src/hooks/useAuthStore.js
+++ 10-calendar-app-vite/src/hooks/useAuthStore.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import calendarApi from '../apis/calendarApi';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store';
 
 export const useAuthStore = () => {
   const dispatch = useDispatch();
@@ -7,10 +8,17 @@ export const useAuthStore = () => {
 
   const startLogin = async ({ email, password }) => {
     try {
-      const resp = await calendarApi.post('/auth', { email, password });
-      console.log({ resp });
+      dispatch(onChecking());
+
+      const { data } = await calendarApi.post('/auth', { email, password });
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
-      console.log(error);
+      dispatch(onLogout('Credenciales incorrectas.'));
+      setTimeout(() => {
+        dispatch(clearErrorMessage);
+      }, 1000);
     }
   };
 
