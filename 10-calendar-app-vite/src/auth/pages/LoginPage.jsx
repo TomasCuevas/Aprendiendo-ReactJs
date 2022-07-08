350ebeaa60c396b457d68bc203e8294aeb967a64
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-08 15:22:19 -0300

    useForm - login y registro 18

--- 10-calendar-app-vite/src/auth/pages/LoginPage.jsx
+++ 10-calendar-app-vite/src/auth/pages/LoginPage.jsx
@@ -1,17 +1,64 @@
+import { useForm } from '../../hooks';
 import './LoginPage.css';
 
 export const LoginPage = () => {
+  const {
+    loginEmail,
+    loginPassword,
+    onInputChange: loginChange,
+  } = useForm({
+    loginEmail: '',
+    loginPassword: '',
+  });
+
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerConfirmPassword,
+    onInputChange: registerChange,
+  } = useForm({
+    registerName: '',
+    registerEmail: '',
+    registerPassword: '',
+    registerConfirmPassword: '',
+  });
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    console.log({ loginEmail, loginPassword });
+  };
+
+  const registerSubmit = (event) => {
+    event.preventDefault();
+    console.log({ registerName, registerEmail, registerPassword, registerConfirmPassword });
+  };
+
   return (
     <div className="container login-container">
       <div className="row">
         <div className="col-md-6 login-form-1">
           <h3>Ingreso</h3>
-          <form>
+          <form onSubmit={loginSubmit}>
             <div className="form-group mb-2">
-              <input type="text" className="form-control" placeholder="Correo" />
+              <input
+                className="form-control"
+                name="loginEmail"
+                onChange={loginChange}
+                placeholder="Correo"
+                type="text"
+                value={loginEmail}
+              />
             </div>
             <div className="form-group mb-2">
-              <input type="password" className="form-control" placeholder="Contraseña" />
+              <input
+                className="form-control"
+                name="loginPassword"
+                onChange={loginChange}
+                placeholder="Contraseña"
+                type="password"
+                value={loginPassword}
+              />
             </div>
             <div className="form-group mb-2">
               <input type="submit" className="btnSubmit" value="Login" />
@@ -21,19 +68,47 @@ export const LoginPage = () => {
 
         <div className="col-md-6 login-form-2">
           <h3>Registro</h3>
-          <form>
+          <form onSubmit={registerSubmit}>
             <div className="form-group mb-2">
-              <input type="text" className="form-control" placeholder="Nombre" />
+              <input
+                className="form-control"
+                name="registerName"
+                onChange={registerChange}
+                placeholder="Nombre"
+                type="text"
+                value={registerName}
+              />
             </div>
             <div className="form-group mb-2">
-              <input type="email" className="form-control" placeholder="Correo" />
+              <input
+                className="form-control"
+                name="registerEmail"
+                onChange={registerChange}
+                placeholder="Correo"
+                type="email"
+                value={registerEmail}
+              />
             </div>
             <div className="form-group mb-2">
-              <input type="password" className="form-control" placeholder="Contraseña" />
+              <input
+                className="form-control"
+                name="registerPassword"
+                onChange={registerChange}
+                placeholder="Contraseña"
+                type="password"
+                value={registerPassword}
+              />
             </div>
 
             <div className="form-group mb-2">
-              <input type="password" className="form-control" placeholder="Repita la contraseña" />
+              <input
+                className="form-control"
+                name="registerConfirmPassword"
+                onChange={registerChange}
+                placeholder="Repita la contraseña"
+                type="password"
+                value={registerConfirmPassword}
+              />
             </div>
 
             <div className="form-group mb-2">
