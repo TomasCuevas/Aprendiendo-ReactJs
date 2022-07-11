f8bc631c176decf55c44f3f8350f7c680173b73f
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-11 10:27:57 -0300

    tarea - onDeleteEvent, onLoadingEvents y onClearCalendar 18

--- 10-calendar-app-vite/tests/__fixtures/calendarStates.js
+++ 10-calendar-app-vite/tests/__fixtures/calendarStates.js
@@ -1,13 +1,13 @@
 export const events = [
   {
-    id: 1,
+    _id: 1,
     start: new Date('2022-7-11 09:43:00'),
     end: new Date('2022-7-11 11:43:00'),
     title: 'Cumpleaños de Pancracio Lopez',
     note: 'Alguna nota.',
   },
   {
-    id: 2,
+    _id: 2,
     start: new Date('2022-7-15 15:43:00'),
     end: new Date('2022-7-15 17:43:00'),
     title: 'Cumpleaños de Mamerta Lopez',
