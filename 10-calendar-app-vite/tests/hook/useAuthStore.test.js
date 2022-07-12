0646c6ebcf809771b47589d08f81729a22ac326e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-12 11:22:12 -0300

    startLogin debe de realizar el login correctamente 18

--- 10-calendar-app-vite/tests/hook/useAuthStore.test.js
+++ 10-calendar-app-vite/tests/hook/useAuthStore.test.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { useAuthStore } from '../../src/hooks/useAuthStore';
 import { authSlice } from '../../src/store';
 import { initialState } from '../__fixtures/authStates';
+import { testUserCredentials } from '../__fixtures/testUser';
 
 /* eslint-disable no-undef */
 const getMockStore = (initialState) => {
@@ -33,5 +34,26 @@ describe('Pruebas en el useAuthStore', () => {
       startLogout: expect.any(Function),
       startRegister: expect.any(Function),
     });
+  });
+
+  test('startLogin debe de realizar el login correctamente', async () => {
+    localStorage.clear();
+    const mockStore = getMockStore(initialState);
+    const { result } = renderHook(() => useAuthStore(), {
+      wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>,
+    });
+
+    await act(async () => {
+      await result.current.startLogin(testUserCredentials);
+    });
+
+    const { errorMessage, status, user } = result.current;
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: undefined,
+      status: 'authenticated',
+      user: { name: testUserCredentials.name, _id: testUserCredentials._id },
+    });
+    expect(localStorage.getItem('token')).toEqual(expect.any(String));
+    expect(localStorage.getItem('token-init-date')).toEqual(expect.any(String));
   });
 });
