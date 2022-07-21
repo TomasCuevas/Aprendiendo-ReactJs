8d83a30f273c7120977dd544e80a620c6f709dc2
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-21 10:36:05 -0300

    useRef y TimeLines

--- 11-bases/src/bases/CounterEffect.tsx
+++ 11-bases/src/bases/CounterEffect.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
 
 export const CounterEffect = () => {
   const [counter, setCounter] = useState(5);
+  const counterElement = useRef<HTMLHeadingElement>(null);
 
   const onIncrement = () => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
@@ -15,15 +16,17 @@ export const CounterEffect = () => {
 
     console.log('%cSe llego al valor maximo', 'color: red; background-color: black');
 
-    gsap.to('h2', { y: -10, duration: 0.5 }).then(() => {
-      gsap.to('h2', { y: 10, duration: 0.2, ease: 'bounce.out' });
-    });
+    const timeline = gsap.timeline();
+
+    timeline
+      .to(counterElement.current, { y: -10, duration: 0.5 })
+      .to(counterElement.current, { y: 10, duration: 0.2, ease: 'bounce.out' });
   }, [counter]);
 
   return (
     <>
       <h1>CounterEffect:</h1>
-      <h2>{counter}</h2>
+      <h2 ref={counterElement}>{counter}</h2>
 
       <button onClick={onIncrement}>+1</button>
     </>
