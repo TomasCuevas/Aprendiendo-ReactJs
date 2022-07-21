3bd45100f2aabdc89e270417b87f47c2be1c6bd9
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-21 11:38:59 -0300

    Separar acciones, interfaces y reducer

--- 11-bases/src/bases/CounterReducer.tsx
+++ 11-bases/src/bases/CounterReducer.tsx
@@ -1,34 +1,12 @@
 import { useReducer } from 'react';
 
-interface CounterState {
-  counter: number;
-  previous: number;
-  changes: number;
-}
+import { counterReducer } from '../reducers/counterReducer/counterReducer';
+import { CounterState } from '../reducers/counterReducer/interfaces/counterInterfaces';
 
 const INITIAL_STATE: CounterState = {
   counter: 0,
   previous: 0,
   changes: 0,
-};
-
-type CounterAction = { type: 'increaseBy'; payload: { value: number } } | { type: 'reset' };
-
-const counterReducer = (state: CounterState, action: CounterAction): CounterState => {
-  switch (action?.type) {
-    case 'increaseBy':
-      return {
-        counter: state.counter + action.payload.value,
-        previous: state.counter,
-        changes: state.changes + 1,
-      };
-
-    case 'reset':
-      return INITIAL_STATE;
-
-    default:
-      return state;
-  }
 };
 
 export const CounterReducerComponent = () => {
---
+++ 11-bases/src/reducers/counterReducer/state/counterState.ts
@@ -0,0 +1,7 @@
+import { CounterState } from '../interfaces/counterInterfaces';
+
+export const INITIAL_STATE: CounterState = {
+  counter: 0,
+  previous: 0,
+  changes: 0,
+};
