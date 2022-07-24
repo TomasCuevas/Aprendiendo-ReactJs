e5611f0fe303ab3a43f72b62d52670103b25ee5e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-23 23:03:57 -0300

    Recibir props al componente hijo

--- 12-react-app/src/02-component-patterns/components/ProductCard.tsx
+++ 12-react-app/src/02-component-patterns/components/ProductCard.tsx
@@ -3,15 +3,28 @@ import { useProduct } from '../hooks/useProduct';
 import Styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 
-export const ProductCard = () => {
+interface Props {
+  product: Product;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+export const ProductCard = ({ product }: Props) => {
   const { counter, increaseBy } = useProduct();
 
   return (
     <div className={Styles.productCard}>
-      {/* <img src="./coffee-mug.png" alt="Coffee Mug" className={Styles.productImg} /> */}
-      <img src={noImage} alt="Coffee Mug" className={Styles.productImg} />
+      {product.img ? (
+        <img src={product.img} alt={product.title} className={Styles.productImg} />
+      ) : (
+        <img src={noImage} alt="No Image" className={Styles.productImg} />
+      )}
 
-      <span className={Styles.productDescription}>Coffee Mug</span>
+      <span className={Styles.productDescription}>{product.title}</span>
       <div className={Styles.buttonsContainer}>
         <button onClick={() => increaseBy(-1)} className={Styles.buttonMinus}>
           -
