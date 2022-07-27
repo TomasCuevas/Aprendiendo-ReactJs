4b729002d2d14cd49fe32e2c764f97bf8aa2fb86
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-27 08:44:45 -0300

    Continuación de la sección

--- 12-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 12-react-app/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,48 +1,22 @@
 import { ProductCard, ProductButtons, ProductImage, ProductTitle } from '../components';
 
-import { useShoppingCart } from '../hooks/useShoppingCart';
-
 import { products } from '../data/products';
 import '../styles/customStyles.css';
 
-export const ShoppingPage = () => {
-  const { shoppingCart, onProductCountChange } = useShoppingCart();
+const product = products[0];
 
+export const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
 
       <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        {products.map((product) => (
-          <ProductCard
-            className="bg__dark"
-            key={product.id}
-            onChange={onProductCountChange}
-            product={product}
-            value={shoppingCart[product.id]?.count || 0}
-          >
-            <ProductCard.Image className="custom__image" />
-            <ProductCard.Title className="text__white text__bold" />
-            <ProductCard.Buttons className="custom__buttons" />
-          </ProductCard>
-        ))}
-      </div>
-
-      <div className="shopping__card">
-        {Object.entries(shoppingCart).map(([key, product]) => (
-          <ProductCard
-            className="bg__dark"
-            key={key}
-            onChange={onProductCountChange}
-            product={product}
-            styles={{ width: '120px' }}
-            value={product.count}
-          >
-            <ProductCard.Image className="custom__image" />
-            <ProductCard.Buttons className="custom__buttons" />
-          </ProductCard>
-        ))}
+        <ProductCard className="bg__dark" key={product.id} product={product}>
+          <ProductCard.Image className="custom__image" />
+          <ProductCard.Title className="text__white text__bold" />
+          <ProductCard.Buttons className="custom__buttons" />
+        </ProductCard>
       </div>
     </div>
   );
