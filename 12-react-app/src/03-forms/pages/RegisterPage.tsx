234ff6be959968a828976d2395b81460fe99707c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-11 14:12:43 -0300

    Formulario tradicional - con useState

--- 12-react-app/src/03-forms/pages/RegisterPage.tsx
+++ 12-react-app/src/03-forms/pages/RegisterPage.tsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
+
+  const { name, email, password, repeatPassword } = registerData;
+
+  const onChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setRegisterData({
+      ...registerData,
+      [target.name]: target.value,
+    });
+  };
+
   return (
     <div>
       <h1>RegisterPage</h1>
 
       <form>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Repeat Password" />
+        <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
+        <input type="email" placeholder="Email" name="email" value={email} onChange={onChange} />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          name="repeatPassword"
+          value={repeatPassword}
+          onChange={onChange}
+        />
         <button type="submit">Create</button>
       </form>
     </div>
