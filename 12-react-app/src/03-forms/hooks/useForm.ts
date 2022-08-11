7371cb1a801cf03fd1afed79c2c95a11c582b36e
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-11 16:18:28 -0300

    Expandir funcionalidad de nuestro custom hook

--- 12-react-app/src/03-forms/hooks/useForm.ts
+++ 12-react-app/src/03-forms/hooks/useForm.ts
@@ -10,12 +10,23 @@ export const useForm = <T>(initialData: T) => {
     });
   };
 
+  const resetForm = () => setFormData(initialData);
+
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return re.test(email);
+  };
+
   return {
     // properties
     ...formData,
     formData,
 
     // methods
+    isValidEmail,
     onChange,
+    resetForm,
   };
 };
--- 12-react-app/src/03-forms/pages/RegisterPage.tsx
+++ 12-react-app/src/03-forms/pages/RegisterPage.tsx
@@ -4,14 +4,21 @@ import { useForm } from '../hooks/useForm';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
-  const { formData, name, email, password, repeatPassword, onChange } = useForm(
-    {
-      name: '',
-      email: '',
-      password: '',
-      repeatPassword: '',
-    },
-  );
+  const {
+    formData,
+    name,
+    email,
+    password,
+    repeatPassword,
+    onChange,
+    resetForm,
+    isValidEmail,
+  } = useForm({
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -29,29 +36,49 @@ export const RegisterPage = () => {
           name="name"
           value={name}
           onChange={onChange}
+          className={name.trim().length < 1 ? 'has-error' : ''}
         />
+        {name.trim().length < 1 && <span>Este campo en obligatorio.</span>}
         <input
           type="email"
           placeholder="Email"
           name="email"
           value={email}
           onChange={onChange}
+          className={!isValidEmail(email) ? 'has-error' : ''}
         />
+        {!isValidEmail(email) && <span>Este campo en obligatorio.</span>}
         <input
           type="password"
           placeholder="Password"
           name="password"
           value={password}
           onChange={onChange}
+          className={password.length < 8 ? 'has-error' : ''}
         />
+        {password.length < 8 && (
+          <span>El password debe tener minimo 8 caracteres.</span>
+        )}
         <input
           type="password"
           placeholder="Repeat Password"
           name="repeatPassword"
           value={repeatPassword}
           onChange={onChange}
+          className={
+            repeatPassword != password || repeatPassword.length < 8
+              ? 'has-error'
+              : ''
+          }
         />
+        {repeatPassword !== password ||
+          (repeatPassword.length < 8 && (
+            <span>Las contraseñas no coinciden.</span>
+          ))}
         <button type="submit">Create</button>
+        <button type="button" onClick={resetForm}>
+          Reset Form
+        </button>
       </form>
     </div>
   );
