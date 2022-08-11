ad32558b3da7db0a96aef8f0275f0110dc8b7c9b
Tomas Cuevas <memo_mister398@hotmail.com>
2022-08-11 16:49:39 -0300

    Formik - Obtener información del formulario

--- 12-react-app/src/03-forms/pages/FormikBasicPage.tsx
+++ 12-react-app/src/03-forms/pages/FormikBasicPage.tsx
@@ -1,25 +1,59 @@
+import { useFormik } from 'formik';
+
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
 
-      <form noValidate>
+      <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" />
+        <input
+          name="firstName"
+          onChange={handleChange}
+          type="text"
+          value={values.firstName}
+        />
         <span>First name is required</span>
 
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" />
+        <input
+          name="lastName"
+          onChange={handleChange}
+          type="text"
+          value={values.lastName}
+        />
         <span>Last name is required</span>
 
         <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+        <input
+          name="email"
+          onChange={handleChange}
+          type="email"
+          value={values.email}
+        />
         <span>Email is required</span>
 
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          name="password"
+          onChange={handleChange}
+          type="password"
+          value={values.password}
+        />
         <span>Password is required</span>
 
         <button type="submit">Register</button>
